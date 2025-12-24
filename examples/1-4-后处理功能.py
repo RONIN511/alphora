8f76b38354b698761d48b0376aaa19428a8f1dfc
tiantian <tiantianit@chinamobile.com>
2025-12-25 00:09:44 +0800

    1

--- examples/1-4-后处理功能.py
+++ examples/1-4-后处理功能.py
@@ -12,37 +12,36 @@
 import asyncio
 from alphora.agent.base import BaseAgent
 from alphora.models.llms import OpenAILike
-from alphora.postprocess import JsonKeyExtractorPP, FilterPP, ReplacePP
+from alphora.postprocess import JsonKeyExtractorPP, ReplacePP
 from alphora.memory import BaseMemory
 
 
-class ProductInfoAgent(BaseAgent):
+class PostProcessAgent(BaseAgent):
     """产品信息智能体，返回产品的JSON格式信息"""
 
-    async def product_info(self, product_name):
-        """获取产品信息"""
-        prompt = "请提供关于{{ product_name }}的详细信息，包括：名称、价格、分类和描述。"
-        prompt += "请以JSON格式返回，包含name、price、category和description字段。"
+    async def sql_coder(self, query: str, school_name: str):
 
-        json_pp = JsonKeyExtractorPP(target_key="description",
-                                     stream_only_target=False,
+        prompt = ("请编写用户问题的SQL脚本（表名: sch_info, 列名: school, score, city, prov, is_211, is_985），"
+                  "其中如涉及学校名称相关必须预留学校名称为 PLACEHOLDER，问题:{{ query }}，用json写，包含 sql, explain 两个key")
+
+        replace_pp = ReplacePP(replace_map={"PLACEHOLDER": school_name})
+
+        json_pp = JsonKeyExtractorPP(target_key="explain",
+                                     stream_only_target=True,
                                      response_only_target=True)
 
-        replace_pp = ReplacePP(replace_map={"苹果": "华为"})
+        # 多个后处理可进行级联
+        complex_pp = json_pp >> replace_pp
 
-        complex_pp = replace_pp >> json_pp
-        
         prompter = self.create_prompt(prompt=prompt)
-        prompter.update_placeholder(product_name=product_name)
-        resp = await prompter.acall(is_stream=True, force_json=True, postprocessor=json_pp)
-        print('完整输出：\n', resp)
+        resp = await prompter.acall(query=query, is_stream=True, postprocessor=complex_pp)
         return resp
 
 
 async def main():
     print("===== 后处理功能示例 =====\n")
 
-    llm_api_key: str = 'sk-68ac5f5ccf3540ba834deeeaecb48987'  # 替换为您的API密钥
+    llm_api_key: str = 'sk-68ac5f5ccf3540ba834deeeaecb48987'
     llm_base_url: str = "https://dashscope.aliyuncs.com/compatible-mode/v1"
     llm_model_name: str = "qwen-plus"
     
@@ -52,15 +51,11 @@ async def main():
         base_url=llm_base_url,
         model_name=llm_model_name
     )
-    
-    # 1. JSON键提取示例
-    print("1. JSON键提取示例")
-    print("=" * 40)
-    
-    product_agent = ProductInfoAgent(llm=llm)
 
-    # 获取产品信息
-    response = await product_agent.product_info("苹果手机")
+    pp_agent = PostProcessAgent(llm=llm)
+
+    _ = await pp_agent.sql_coder(query='查询我学校是不是985', school_name='北京大学')
+
     pass
 
 
